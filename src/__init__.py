bcf2e881a136aae613f4c0ab18b4edb3cf56a172
Raghu Prakash.C <raghuprakash56@gmail.com>
2026-06-09 17:38:37 +0530

    Add package initialization file

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+RF Communication with LoRa
+
+A comprehensive project for RF (Radio Frequency) communication using LoRa technology.
+"""
+
+__version__ = "0.1.0"
+__author__ = "raghuprakash56-glitch"
+__license__ = "MIT"
+
+# Package initialization
